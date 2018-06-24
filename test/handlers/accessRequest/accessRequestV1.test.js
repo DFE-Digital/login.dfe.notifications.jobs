69ae4e8eb30d1f3f68dc7d379d4752fd45b2e037
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-06-24 20:27:42 +0100

    Add accessRequest handler for emails

---
+++ test/handlers/accessRequest/accessRequestV1.test.js
@@ -0,0 +1,89 @@
+jest.mock('./../../../lib/infrastructure/email');
+
+const { getEmailAdapter } = require('./../../../lib/infrastructure/email');
+const { getHandler } = require('./../../../lib/handlers/accessRequest/accessRequestV1');
+const emailSend = jest.fn();
+
+const config = {
+  notifications: {
+    profileUrl: 'https://profile.dfe.signin',
+  },
+};
+const logger = {};
+const jobData = {
+  orgName: 'Test Organisation',
+  firstName: 'Test',
+  lastName: 'One',
+  approved: 'true',
+  reason: 'test',
+};
+
+describe('When handling accessRequest_v1 job', () => {
+  beforeEach(() => {
+    emailSend.mockReset();
+
+    getEmailAdapter.mockReset();
+    getEmailAdapter.mockReturnValue({ send: emailSend });
+  });
+
+  it('then it should return a handler with a processor', () => {
+    const handler = getHandler(config, logger);
+
+    expect(handler).not.toBeNull();
+    expect(handler.type).toBe('accessrequest_v1');
+    expect(handler.processor).not.toBeNull();
+    expect(handler.processor).toBeInstanceOf(Function);
+  });
+
+  it('then it should get email adapter with supplied config and logger', async () => {
+    const handler = getHandler(config, logger);
+
+    await handler.processor(jobData);
+
+    expect(getEmailAdapter.mock.calls).toHaveLength(1);
+    expect(getEmailAdapter.mock.calls[0][0]).toBe(config);
+    expect(getEmailAdapter.mock.calls[0][1]).toBe(logger);
+  });
+
+  it('then it should send email to users email address', async () => {
+    const handler = getHandler(config, logger);
+
+    await handler.processor(jobData);
+
+    expect(emailSend.mock.calls).toHaveLength(1);
+    expect(emailSend.mock.calls[0][0]).toBe(jobData.email);
+  });
+
+  it('then it should send email using access-request-email template', async () => {
+    const handler = getHandler(config, logger);
+
+    await handler.processor(jobData);
+
+    expect(emailSend.mock.calls).toHaveLength(1);
+    expect(emailSend.mock.calls[0][1]).toBe('access-request-email');
+  });
+
+  it('then it should send email using request data as model', async () => {
+    const handler = getHandler(config, logger);
+
+    await handler.processor(jobData);
+
+    expect(emailSend.mock.calls).toHaveLength(1);
+    expect(emailSend.mock.calls[0][2]).toEqual({
+      approved: jobData.approved,
+      reason: jobData.reason,
+      firstName: jobData.firstName,
+      lastName: jobData.lastName,
+      orgName: jobData.orgName,
+    });
+  });
+
+  it('then it should send email with subject', async () => {
+    const handler = getHandler(config, logger);
+
+    await handler.processor(jobData);
+
+    expect(emailSend.mock.calls).toHaveLength(1);
+    expect(emailSend.mock.calls[0][3]).toBe(`Request to access ${jobData.orgName}`);
+  });
+});
