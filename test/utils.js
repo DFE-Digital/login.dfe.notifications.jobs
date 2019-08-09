f2306ca6e764ec7320ed40a49c80dedbf2f1abd6
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-08-09 14:51:05 +0100

    NSA-2237- org request

--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,8 @@
 const getDefaultConfig = () => {
   return {
+    queueStorage: {
+      connectionString: 'redis://test:6379',
+    },
     loggerSettings: {
       logLevel: 'info',
       coors: {
@@ -16,6 +19,47 @@ const getDefaultConfig = () => {
   };
 };
 
+const getOrganisationsClientMock = () => {
+  return {
+    getOrgRequestById: jest.fn(),
+    getApproversForOrganisation: jest.fn(),
+    getOrganisationById: jest.fn(),
+    mockResetAll: function () {
+      this.getOrgRequestById.mockReset();
+      this.getApproversForOrganisation.mockReset();
+      this.getOrganisationById.mockReset();
+    },
+  };
+};
+
+const getLoggerMock = () => {
+  return {
+    info: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+    mockResetAll: function() {
+      this.info.mockReset();
+      this.warn.mockReset();
+      this.error.mockReset();
+    },
+  };
+};
+
+const getDirectoriesClientMock = () => {
+  return {
+    getUsersByIds: jest.fn(),
+    getById: jest.fn(),
+    mockResetAll: function () {
+      this.getUsersByIds.mockReset();
+      this.getById.mockReset();
+
+    },
+  };
+};
+
 module.exports = {
   getDefaultConfig,
+  getDirectoriesClientMock,
+  getOrganisationsClientMock,
+  getLoggerMock,
 };
